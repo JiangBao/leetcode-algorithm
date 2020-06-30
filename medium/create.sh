99667a64cd2d3f6a5781583b06243f1add58b3b0
JiangBao <jiangbao1123@gmail.com>
2020-06-30 15:14:25 +0800

    add create script

---
+++ medium/create.sh
@@ -0,0 +1,9 @@
+set -e
+echo "add new medium Problem: "
+read NAME
+
+mkdir $NAME
+echo -e "# $NAME\n\n## 问题\n\n## 思路\n\n[传送门]()" > "$NAME/README.md"
+touch $NAME/index.js
+
+echo "add new problem success"

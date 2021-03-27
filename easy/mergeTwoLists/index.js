3725f0edc4c7fb30049e54715333970bb8cd6dff
JiangBao <jiangbao1123@gmail.com>
2021-03-27 21:56:14 +0800

    add problem

---
+++ easy/mergeTwoLists/index.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+  let dum = new ListNode(0);
+  let curr = dum;
+
+  while (l1 !== null && l2 !== null) {
+    if (l1.val < l2.val) {
+      curr.next = l1;
+      l1 = l1.next;
+    } else {
+      curr.next = l2;
+      l2 = l2.next;
+    }
+    curr = curr.next;
+  }
+
+  curr.next = l1 !== null ? l1 : l2;
+
+  return dum.next;
+};
